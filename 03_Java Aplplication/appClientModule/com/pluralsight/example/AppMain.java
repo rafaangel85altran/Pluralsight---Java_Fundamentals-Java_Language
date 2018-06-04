35a517f29eae575420e0b2d669fe203779a1b5da
win7 <win7@win7-PC>
2018-06-04 18:05:47 +0200

    New proj for the module 04

---
+++ 03_Java Aplplication/appClientModule/com/pluralsight/example/AppMain.java
@@ -0,0 +1,9 @@
+package com.pluralsight.example;
+
+public class AppMain {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World AppMain");
+	}
+	
+}
--- 03_Java Aplplication/appClientModule/com/pluralsight/example/Newclass.java
+++
@@ -1,5 +0,0 @@
-package com.pluralsight.example;
-
-public class Newclass {
-	//This is a new package
-}
