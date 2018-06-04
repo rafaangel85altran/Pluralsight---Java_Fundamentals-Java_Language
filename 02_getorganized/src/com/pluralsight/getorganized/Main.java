526c68487b5502c38e7060dd8547c2c7cdcf7627
win7 <win7@win7-PC>
2018-06-04 11:28:20 +0200

    Package structure

---
+++ 02_getorganized/src/com/pluralsight/getorganized/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight.getorganized;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World again! 2");
+	}
+	
+}
---
+++ 03_Java Aplplication/appClientModule/com/pluralsight/example/Newclass.java
@@ -0,0 +1,5 @@
+package com.pluralsight.example;
+
+public class Newclass {
+	//This is a new package
+}
