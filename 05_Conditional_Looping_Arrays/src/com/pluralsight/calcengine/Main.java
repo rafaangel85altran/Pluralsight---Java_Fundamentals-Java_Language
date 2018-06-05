ddfd390407c83b31b2e239a2dcc472eefc56e17a
win7 <win7@win7-PC>
2018-06-05 20:16:14 +0200

    Calc engine with switch and arrays

--- 05_Conditional_Looping_Arrays/src/com/pluralsight/calcengine/Main.java
+++ 05_Conditional_Looping_Arrays/src/com/pluralsight/calcengine/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	
 	public static void main (String[] args) {
 		
+		double [] vals = {0, 0};
 		double val1 = 0;
 		double val2 = 0;
 		double result = 0;
@@ -22,47 +23,64 @@ public class Main {
 		
 		char opCode = keyboard.nextLine().charAt(0);
 		
-		if (opCode == '+') {
-			System.out.println("Type first operand");
-			val1 = keyboard.nextDouble();
-			
-			System.out.println("Type second operand");
-			val2 = keyboard.nextDouble();
-			
-			System.out.println("Result is");
-			System.out.println(val1 + val2);
-		}
-		else if (opCode == '-') {
-			System.out.println("Type first operand");
-			val1 = keyboard.nextDouble();
-			
-			System.out.println("Type second operand");
-			val2 = keyboard.nextDouble();
-			
-			System.out.println("Result is");
-			System.out.println(val1 - val2);
-		}
-		else if (opCode == '*') {
-			System.out.println("Type first operand");
-			val1 = keyboard.nextDouble();
-			
-			System.out.println("Type second operand");
-			val2 = keyboard.nextDouble();
-			
-			System.out.println("Result is");
-			System.out.println(val1 * val2);
-		}
-		else if (opCode == '/') {
-			System.out.println("Type first operand");
-			val1 = keyboard.nextDouble();
-			
-			System.out.println("Type second operand");
-			val2 = keyboard.nextDouble();
-			
-			System.out.println("Result is");
-			System.out.println(val1 / val2);
-		}
+		switch (opCode) {
 		
+			case '+':
+			{
+				System.out.println("Type first operand");
+				vals[0] = keyboard.nextDouble();
+				
+				System.out.println("Type second operand");
+				vals[1] = keyboard.nextDouble();
+				
+				System.out.println("Result is");
+				System.out.println(vals[0] + vals[1]);
+				
+				break;
+			}
+			
+			case '-':
+			{
+				System.out.println("Type first operand");
+				val1 = keyboard.nextDouble();
+				
+				System.out.println("Type second operand");
+				val2 = keyboard.nextDouble();
+				
+				System.out.println("Result is");
+				System.out.println(val1 - val2);
+				
+				break;
+			}
+			
+			case '*':
+			{
+				System.out.println("Type first operand");
+				val1 = keyboard.nextDouble();
+				
+				System.out.println("Type second operand");
+				val2 = keyboard.nextDouble();
+				
+				System.out.println("Result is");
+				System.out.println(val1 * val2);
+				
+				break;
+			}
+			
+			case '/':
+			{
+				System.out.println("Type first operand");
+				val1 = keyboard.nextDouble();
+				
+				System.out.println("Type second operand");
+				val2 = keyboard.nextDouble();
+				
+				System.out.println("Result is");
+				System.out.println(val1 / val2);
+				
+				break;
+			}
+		}
+		System.out.println("End of the program");
 	}
-
 }
---
+++ 05_Conditional_Looping_Arrays/src/com/pluralsight/calcengine/functions.java
@@ -0,0 +1,8 @@
+package com.pluralsight.calcengine;
+
+public class functions {
+	public double addition (double val1, double val2) {
+		double result = val1 + val2;
+		return result;
+	}
+}
